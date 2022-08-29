00730d6aec724ae7d16ef5d68c07e7696c91a2ac
Kane Mathis <admin@kanemathis.com>
2022-08-29 17:56:21 -0400

    this works now

--- javascript/fibonacci.js
+++ javascript/fibonacci.js
@@ -1,9 +1,34 @@
 function fibonacci(num) {
-  // type your code here
+  let x=0;
+  let y=1;
+  let sum=x+y;
+  if (num===0){
+    return 0
+  } 
+  else {
+    for (times=2; times<num; times++){
+      x=y;
+      y=sum;
+      sum=x+y;
+    }
+    return sum;
+  }
+
 }
 
+
+
 if (require.main === module) {
-  // add your own tests in here
+
+  console.log("Expecting: 233");
+  console.log("=>", fibonacci(13));
+  
+  console.log("");
+  console.log("Expecting: 2");
+  console.log("=>", fibonacci(3));
+
+  console.log("");
+
   console.log("Expecting: 0");
   console.log("=>", fibonacci(0));
 
@@ -20,5 +45,24 @@ if (require.main === module) {
 
 module.exports = fibonacci;
 
-// Please add your pseudocode to this file
-// And a written explanation of your solution
+// I am planning to set up a calculation for the Fibbinocci sequence that uses the sequence of deriving a number from the last two of the sequence and having it perform that math task the specified amount of times. 
+
+
+// the formula
+// x=y;
+//   y=sum;
+//   sum=x+y;
+//   console.log(sum);
+//   x=y;
+//   y=sum;
+//   sum=x+y;
+//   console.log(sum);
+//   x=y;
+//   y=sum;
+//   sum=x+y;
+//   console.log(sum);
+//   x=y;
+//   y=sum;
+//   sum=x+y;
+//   console.log(sum);
+
